63742c9dd17bcf9dbfce86e86c8868c5634e612c
kaaang <kaaang@kakao.com>
2022-11-26 01:20:39 +0900

    asdf

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,4 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("asdf");
     }
+
+    public void test(){
+        System.out.println("check branch");
+    }
 }
